68f3f13ce8b103fded7417cf4f99612bddfb5e24
hackartists <hackartists@gmail.com>
2024-11-08 15:43:55 +0900

    add testing with two users

--- tests/nft.spec.js
+++ tests/nft.spec.js
@@ -1,39 +1,86 @@
 import { test, expect } from "@playwright/test";
+import { chromium } from "playwright";
 import {
-    image_path,
-    latency,
-    agit_name,
-    collection_name,
-    nft_name,
+  image_path,
+  latency,
+  agit_name,
+  collection_name,
+  nft_name,
 } from "./constants";
 import path from "path";
 
-
 test.describe("NFT test", () => {
-    test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/");
     await page.waitForTimeout(latency);
-    });
+  });
 
-test("veiw original NFT contents", async ({ page }) => {
+  test("Veiw original NFT contents", async ({ page }) => {
     await page
-    .locator('xpath=//*[@id="main"]/div[1]/div[4]/header/div/div[2]/div')
-    .click();
+      .locator('xpath=//*[@id="main"]/div[1]/div[4]/header/div/div[2]/div')
+      .click();
     await page.waitForTimeout(latency);
     await page.getByText(nft_name, { exact: true }).click();
     await page.waitForTimeout(latency);
     await page.getByRole("button", { name: "View Original" }).click();
     await page.waitForTimeout(latency);
-});
+  });
 
-test("sell NFT", async ({ page }) => {
+  test("Sell NFT", async ({ page }) => {
     await page
-    .locator('xpath=//*[@id="main"]/div[1]/div[4]/header/div/div[2]/div')
-    .click();
+      .locator('xpath=//*[@id="main"]/div[1]/div[4]/header/div/div[2]/div')
+      .click();
     await page.waitForTimeout(latency);
     await page.getByText(nft_name, { exact: true }).click();
     await page.waitForTimeout(latency);
     await page.getByRole("button", { name: "Sell Nft" }).click();
     await page.waitForTimeout(latency);
-});
+  });
+
+  test("Buy NFT", async ({ page }) => {
+    const browser = chromium.launch();
+    const context = await browser.newContext({
+      storageState: {
+        origins: [
+          {
+            origin: process.env.BASE_URL || "",
+            localStorage: [
+              {
+                name: "identity",
+                value: `"${process.env.IDENTITY2}"` || "",
+              },
+            ],
+          },
+        ],
+      },
+    });
+    const page = await context.newPage();
+    // TODO: TEST with another user
+  });
+
+  test("View original NFT by a new owner", async ({ page }) => {
+    const browser = chromium.launch();
+    const context = await browser.newContext({
+      storageState: {
+        origins: [
+          {
+            origin: process.env.BASE_URL || "",
+            localStorage: [
+              {
+                name: "identity",
+                value: `"${process.env.IDENTITY2}"` || "",
+              },
+            ],
+          },
+        ],
+      },
+    });
+    const page = await context.newPage();
+
+    // TODO: TEST with another user
+  });
+
+  test("Failed to view original NFT by a previouse owner", async ({ page }) => {
+    // TODO: TEST with original user
+  });
 });
